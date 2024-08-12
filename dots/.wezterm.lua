c11277ea2d34222097a3f609b47fcc295a2af2c9
pauldo <admin@pauldo.me>
2024-08-12 23:09:55 +0800

    fix

--- dots/.wezterm.lua
+++ dots/.wezterm.lua
@@ -22,6 +22,9 @@ config.hide_tab_bar_if_only_one_tab = true
 --   saturation = 1.0,
 -- }
 
+config.font = wezterm.font 'ComicShannsMono Nerd Font Mono'
+config.font_size = 16.0
+
 config.text_background_opacity = 0.65
 
 
