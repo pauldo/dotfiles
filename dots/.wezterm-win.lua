3e2cdece051bee46e4e475be80805bb8cba81c82
cheems <490001304@qq.com>
2024-08-12 10:31:17 +0800

    fix

--- dots/.wezterm-win.lua
+++ dots/.wezterm-win.lua
@@ -15,10 +15,12 @@ config.wsl_domains = {
 }
 config.default_domain = 'WSL:Arch'
 
-
 local config = wezterm.config_builder()
 
 config.default_prog = { 'wsl', '-d', 'Arch' }
+
+config.font = wezterm.font 'ComicShannsMono Nerd Font Mono'
+config.font_size = 10.0
 
 -- config.color_scheme = 'Batman'
 
@@ -30,21 +32,11 @@ config.keys = {
   },
 }
 
-
 config.hide_tab_bar_if_only_one_tab = true
 
--- config.window_background_image = '/Users/paul/.dotfiles/nix/os/modules/hyprland/catgirl.jpg'
+config.text_background_opacity = 0.85
 
--- config.window_background_image_hsb = {
---   brightness = 0.05,
---   hue = 1.0,
---   saturation = 1.0,
--- }
-
-config.text_background_opacity = 0.65
-
-
-local dimmer = { brightness = 0.1 }
+local dimmer = { brightness = 0.05 }
 
 config.enable_scroll_bar = true
 config.min_scroll_bar_height = '2cell'
